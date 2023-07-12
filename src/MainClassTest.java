90c3eb209b2778d8550d4cda1eb7e5550dc9aebe
movchinnikov <dikushi@yandex.com>
2023-07-12 22:04:45 +0400

    Тест 3

--- src/MainClassTest.java
+++ src/MainClassTest.java
@@ -6,6 +6,12 @@ public class MainClassTest {
     private final MainClass mainClass = new MainClass();
 
     @Test
+    public void testGetClassString() {
+        Assert.assertTrue("Метод getClassString не содержит подстроки 'hello' или 'Hello'",
+                mainClass.getClassString().contains("hello") || mainClass.getClassString().contains("Hello"));
+    }
+
+    @Test
     public void testGetClassNumber() {
         Assert.assertTrue("Значение метода getClassNumber меньше 45", mainClass.getClassNumber() > 45);
     }
