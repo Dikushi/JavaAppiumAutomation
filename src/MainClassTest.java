fd86859c35c174738a2f7c436e8987a87954e9c9
movchinnikov <dikushi@yandex.com>
2023-07-12 21:54:28 +0400

    Тест 2

--- src/MainClassTest.java
+++ src/MainClassTest.java
@@ -6,6 +6,11 @@ public class MainClassTest {
     private final MainClass mainClass = new MainClass();
 
     @Test
+    public void testGetClassNumber() {
+        Assert.assertTrue("Значение метода getClassNumber меньше 45", mainClass.getClassNumber() > 45);
+    }
+
+    @Test
     public void testGetLocalNumber() {
         Assert.assertEquals("Значение метода getLocalNumber не равно 14",14, mainClass.getLocalNumber());
     }
